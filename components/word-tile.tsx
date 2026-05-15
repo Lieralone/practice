5bfa64d91de79072ecb764e2849163de3925ebb1
Lieralone <91555284+Lieralone@users.noreply.github.com>
2026-05-15 18:37:26 +0800

    Update word-tile.tsx

--- components/word-tile.tsx
+++ components/word-tile.tsx
@@ -32,7 +32,7 @@ export default function WordTile({
     word.language === "kapampangan"
       ? "Kapampangan"
       : word.language === "pangasinense"
-      ? "Pangasinense"
+      ? "Pangasinan"
       : "";
 
   const languageAccentColor =
